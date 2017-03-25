335dead5900398518152de585f88a00628f4e416
Wliu <Wliu1402@gmail.com>
2017-03-25 16:33:59 -0400

    Fix proposal scrollbars

---
+++ proposals.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+	// Set the size of the form to the available window size
+	const form = document.getElementById('form');
+	form.style.height = window.innerHeight + 'px';
+	form.style.width = window.innerWidth + 'px';
+}
