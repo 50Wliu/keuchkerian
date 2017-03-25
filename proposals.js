9af4765dc5fbeed22260a9437d621662d3df0bf9
Wliu <Wliu1402@gmail.com>
2017-03-25 16:37:10 -0400

    Also resize when the window is resized

--- proposals.js
+++ proposals.js
@@ -1,4 +1,13 @@
 window.onload = () => {
+	setFormSize();
+}
+
+window.onresize = () => {
+	setFormSize();
+}
+
+function setFormSize()
+{
 	// Set the size of the form to the available window size
 	const form = document.getElementById('form');
 	form.style.height = window.innerHeight + 'px';
