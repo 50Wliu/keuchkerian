a0ca38fb94dcd28448c6577d7f9b0350027d09c8
acs3ss <acs3ss@virginia.edu>
2017-03-26 10:51:17 -0400

    so punny

--- js/shared.js
+++ js/shared.js
@@ -3,12 +3,15 @@ $(document).ready(function()
 	var text = ["Renewable energy? I'm a big fan",
 	            "Keep an even kale",
 				"Herb your enthusiasm",
+				"Visualize whirled peas",
 				"Air pollution is a mist-demeanor",
 				"Wasting paper cups is high tree sin",
 				"Get green-shirtified",
 				"Q: Why did the leaf go to the doctor? A: Because it was feeling green",
 				"Q: How can you tell the ocean is friendly? A: It waves",
-				"Q: How do trees access the internet? A: They log on"];
+				"Q: How do trees access the internet? A: They log on",
+				"Q: What's the name of the new Tom Cruise eco-thriller? A: Mission Compostable",
+				"Q: Why do potatoes make good detectives? A: Because they keep their eyes peeled."];
 	var x = Math.floor(Math.random() * text.length);
 	$('#pun').html(text[x]);
 });
