d8ef5f4a2dbd84c5239f3183f84893c721ba29d7
Wliu <Wliu1402@gmail.com>
2017-03-26 01:18:11 -0400

    Minor changes to puns

--- js/shared.js
+++ js/shared.js
@@ -1,8 +1,11 @@
 $(document).ready(function()
 {
-	var text = ["Renewable energy? I'm a big fan","Keep an even kale","Herb your enthusiasm","Q: Why did the leaf go to the doctor? A: Because it was feeling green"];
-	var x = Math.floor((Math.random()*4));
-	$('#myTxt').html(text[x]);
+	var text = ["Renewable energy? I'm a big fan",
+	            "Keep an even kale",
+				"Herb your enthusiasm",
+				"Q: Why did the leaf go to the doctor? A: Because it was feeling green"];
+	var x = Math.floor(Math.random() * text.length);
+	$('#pun').html(text[x]);
 });
 
 $(function () {
