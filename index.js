b4b9aff8570cc38b6853bdc58cc216638145719b
xiaosneeze56 <reneelinmitchell@gmail.com>
2017-03-25 22:10:36 -0400

    slfkjasd;kfljasd;kfjasdlfkjas;l

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 // index.js
 
-$(window).scroll(test);
-
-function test() {
+$(window).scroll(function() {
+	// right now this is an anonymous function
+	// because it's within "scroll" it's
+	// automatically gonna get called
 	console.log($(window).scrollTop());
 
-	var breakpoint = 80;
+	// create a variable called breakpoint
+	var breakpoint = 100;
 
 	if ($(window).scrollTop() >= breakpoint){
-		$('.welcomePic1').css('visibility',
+		$('.welcomePic1 img').css('visibility',
 			'visible').hide().fadeIn(1500);
 		// 1500 = 1.5 seconds
 
 		// make this only run once; cause scroll is once
-		$(this).off('scroll');
-	}
-}
+		$(this).off('scroll')
 
-// $('#myCollapsible').on('hidden.bs.collapse', function () {
-//   console.log("hello world");
-// })
+		// activate carousel
+		$('#myCollapsible').on('hidden.bs.collapse', function () {
+	  		// do something…
+		})
+	}
+});
