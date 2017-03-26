e2201374b82476feab2c2d60e49cdefe4c1b097d
acs3ss <acs3ss@virginia.edu>
2017-03-26 04:49:01 -0400

    more puns

--- js/shared.js
+++ js/shared.js
@@ -3,7 +3,12 @@ $(document).ready(function()
 	var text = ["Renewable energy? I'm a big fan",
 	            "Keep an even kale",
 				"Herb your enthusiasm",
-				"Q: Why did the leaf go to the doctor? A: Because it was feeling green"];
+				"Air pollution is a mist-demeanor",
+				"Wasting paper cups is high tree sin",
+				"Get green-shirtified",
+				"Q: Why did the leaf go to the doctor? A: Because it was feeling green",
+				"Q: How can you tell the ocean is friendly? A: It waves",
+				"Q: How do trees access the internet? A: They log on"];
 	var x = Math.floor(Math.random() * text.length);
 	$('#pun').html(text[x]);
 });
