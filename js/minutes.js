38f52f619c5dba3b4caba364557ef2070437419a
acs3ss <acs3ss@virginia.edu>
2017-03-26 07:25:13 -0400

    minutes stuff

---
+++ js/minutes.js
@@ -0,0 +1,18 @@
+readTextFile("file:///C:/Users/student/Documents/GitHub/keuchkerian/minutes.txt");
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
